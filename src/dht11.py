ec73498a96644e4da9012b5320f1db9b23ae7a23
Stefan Sullivan <stefan.sullivan@gmail.com>
2025-05-14 17:05:32 -0700

    Update dht11.py to isolate main

--- src/dht11.py
+++ src/dht11.py
@@ -47,33 +47,34 @@ def DHT11():
     
 
 #main program
-dht_pwr = Pin(14, Pin.OUT)      #connect GPIO 14 to '+' on DHT11
-dht_data = Pin(15, Pin.IN, Pin.PULL_UP) #connect GPIO 15 to 'out' on DHT11
-
-dht_pwr.value(1)                #power on DHT11
-sm=rp2.StateMachine(1)          #create empty state machine
-utime.sleep(2)                  #wait for DHT11 to start up
-
-while True:
-    print('reading')
-    data=[]
-    total=0
-    sm.init(DHT11,freq=1600000,set_base=dht_data,in_base=dht_data,jmp_pin=dht_data) #start state machine
-    #state machine frequency adjusted so that PIO countdown during 'readdata' ends somewhere between the  
-    #duration of a '0' and a '1' high signal
-    sm.active(1)    
-    for i in range(5):         #data should be 40 bits (5 bytes) long
-        data.append(sm.get())  #read byte
+if __name__ == "__main__":
+    dht_pwr = Pin(14, Pin.OUT)      #connect GPIO 14 to '+' on DHT11
+    dht_data = Pin(15, Pin.IN, Pin.PULL_UP) #connect GPIO 15 to 'out' on DHT11
     
-    print("data: " + str(data))
+    dht_pwr.value(1)                #power on DHT11
+    sm=rp2.StateMachine(1)          #create empty state machine
+    utime.sleep(2)                  #wait for DHT11 to start up
     
-    #check checksum (lowest 8 bits of the sum of the first 4 bytes)
-    for i in range(4):
-        total=total+data[i]
-    if((total & 255) == data[4]):
-        humidity=data[0]        #DHT11 provides integer humidity (no decimal part)
-        temperature=(1-2*(data[2] >> 7) )*(data[2] & 0x7f) #DHT11 provides signed integer temperature (no decimal part)
-        print("Humidity: %d%%, Temp: %dC" % (humidity, temperature))        
-    else:
-        print("Checksum: failed")
-    utime.sleep_ms(500)
+    while True:
+        print('reading')
+        data=[]
+        total=0
+        sm.init(DHT11,freq=1600000,set_base=dht_data,in_base=dht_data,jmp_pin=dht_data) #start state machine
+        #state machine frequency adjusted so that PIO countdown during 'readdata' ends somewhere between the  
+        #duration of a '0' and a '1' high signal
+        sm.active(1)    
+        for i in range(5):         #data should be 40 bits (5 bytes) long
+            data.append(sm.get())  #read byte
+        
+        print("data: " + str(data))
+        
+        #check checksum (lowest 8 bits of the sum of the first 4 bytes)
+        for i in range(4):
+            total=total+data[i]
+        if((total & 255) == data[4]):
+            humidity=data[0]        #DHT11 provides integer humidity (no decimal part)
+            temperature=(1-2*(data[2] >> 7) )*(data[2] & 0x7f) #DHT11 provides signed integer temperature (no decimal part)
+            print("Humidity: %d%%, Temp: %dC" % (humidity, temperature))        
+        else:
+            print("Checksum: failed")
+        utime.sleep_ms(500)
